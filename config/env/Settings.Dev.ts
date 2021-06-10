9c0876553293c11b1ac99f86d3692c515b54bc84
Daniel Funseth <daniel.funseth@knowit.se>
2021-06-10 16:40:51 +0200

    use test proxy

--- config/env/Settings.Dev.ts
+++ config/env/Settings.Dev.ts
@@ -4,9 +4,9 @@ export class Settings_Dev extends EnvSettings {
  //CONTENTBACKEND_SITEURL="*utvecklarportal.web.local";
   CONTENTBACKEND_SITEURL = "*test-devportal.*";  
 
-  //CONTENTBACKEND_GRAPHAPI="https://digg-test-graphproxy.azurewebsites.net";
+  CONTENTBACKEND_GRAPHAPI="https://digg-test-graphproxy.azurewebsites.net";
   
-  CONTENTBACKEND_GRAPHAPI="http://localhost:4444";
+  //CONTENTBACKEND_GRAPHAPI="http://localhost:4444";
 
   CANONICAL_URL = "http://localhost:8080";  
 
