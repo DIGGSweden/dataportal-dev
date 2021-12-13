fe025c57169447615219f85d20272a92d6f399d6
Daniel Funseth <daniel.funseth@knowit.se>
2021-12-13 10:38:26 +0100

    plocka bort inloggning, fix språkfel

--- src/i18n.ts
+++ src/i18n.ts
@@ -14,13 +14,13 @@ i18n
   .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    detection:({
-      order:['path', 'localStorage', 'navigator', 'htmlTag'],
-      lookupQuerystring: 'lng',
-      lookupLocalStorage: 'i18nextLng',
-      caches: ['localStorage'],
-      lookupFromPathIndex: 0,
-    }),    
+    // detection:({
+    //   order:['path', 'localStorage', 'navigator', 'htmlTag'],
+    //   lookupQuerystring: 'lng',
+    //   lookupLocalStorage: 'i18nextLng',
+    //   caches: ['localStorage'],
+    //   lookupFromPathIndex: 0,
+    // }),    
     resources: {
       sv: {
         resource:resources_sv,
